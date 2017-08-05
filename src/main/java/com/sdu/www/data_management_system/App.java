d0cf34b08456d440344ae3a068742be2e2c29809
levi <jinzhao1991@yeah.net>
2017-08-05 17:35:53 +0800

    添加日志组件

--- src/main/java/com/sdu/www/data_management_system/App.java
+++ src/main/java/com/sdu/www/data_management_system/App.java
@@ -1,10 +1,16 @@
 package com.sdu.www.data_management_system;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
 /**
  * Created by Administrator on 2017/8/5.
  */
 public class App {
+    static Logger log= LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
-        System.out.println("hello world");
+        log.info("hello world");
     }
 }
