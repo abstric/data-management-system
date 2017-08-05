57bd968771a17fc15da80202f47dff4c40fbe703
levi <jinzhao1991@yeah.net>
2017-08-05 17:12:12 +0800

    hello world

---
+++ src/main/java/com/sdu/www/data_management_system/App.java
@@ -0,0 +1,10 @@
+package com.sdu.www.data_management_system;
+
+/**
+ * Created by Administrator on 2017/8/5.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
